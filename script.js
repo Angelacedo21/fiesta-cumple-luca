4372bd81cfe027301dea92f29a1dcef1dc62c2d2
Angelacedo21 <angelacedo44@gmail.com>
2025-01-19 17:07:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('startBtn').addEventListener('click', function() {
+    document.querySelector('.hero').classList.add('hidden');
+    document.getElementById('gameSection').classList.remove('hidden');
+});
